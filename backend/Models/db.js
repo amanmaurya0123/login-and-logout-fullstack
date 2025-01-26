e19fba529ed2a7b768385801490199fd764e7e90
amanmaurya0123 <118646086+amanmaurya0123@users.noreply.github.com>
2025-01-26 19:42:58 +0530

    Update db.js

--- backend/Models/db.js
+++ backend/Models/db.js
@@ -1,19 +1,18 @@
 const mongoose = require("mongoose");
-require("dotenv").config(); // Load environment variables
+require("dotenv").config(); 
 
-// Ensure the connection string exists
+
 if (!process.env.MONGODB_CONN) {
   console.error("MONGODB_CONN is not defined in the .env file.");
-  process.exit(1); // Exit the process with an error code
+  process.exit(1); 
 }
 
-// Connect to MongoDB
 mongoose
-  .connect(process.env.MONGODB_CONN, { dbName: "mernApp" }) // Specify the database name
+  .connect(process.env.MONGODB_CONN, { dbName: "mernApp" }) 
   .then(() => {
     console.log("MongoDB Connected...");
   })
   .catch((err) => {
     console.error("MongoDB Connection Error: ", err);
-    process.exit(1); // Exit if the connection fails
+    process.exit(1); 
   });
